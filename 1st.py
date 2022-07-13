7ef61264c210aea0b6bbfc03ff24ed6705aa3a18
Sravika2509 <52756330+Sravika2509@users.noreply.github.com>
2022-07-13 14:08:25 +0530

    Update 1st.py

--- 1st.py
+++ 1st.py
@@ -4,5 +4,5 @@ for i in range(1, rows + 1):
     for j in range(1, i + 1):
         # multiplication current column and row
         square = i * j
-        print(i * j, end='  ')
+        print(i * j, end=' ')
     print()
