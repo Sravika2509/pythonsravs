311733628bdf8ac4c472349205719f04c2355f8b
Sravika2509 <52756330+Sravika2509@users.noreply.github.com>
2022-07-13 14:07:11 +0530

    Update 1st.py

--- 1st.py
+++ 1st.py
@@ -1,11 +1,8 @@
-import os
-rows = 6
-for i in range(1, rows):
-    num = 1
-    for j in range(rows, 0, -1):
-        if j > i:
-            print(" ", end=' ')
-        else:
-            print(num, end=' ')
-            num += 1
-    print("")
+rows = 8
+# rows = int(input("Enter the number of rows "))
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        # multiplication current column and row
+        square = i * j
+        print(i * j, end='  ')
+    print()
