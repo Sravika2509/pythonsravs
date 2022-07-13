baf56cb1b71ee63cb87120bec041ecf03678dfbf
Sravika2509 <52756330+Sravika2509@users.noreply.github.com>
2022-07-13 14:00:50 +0530

    Update 1st.py

--- 1st.py
+++ 1st.py
@@ -1,7 +1,11 @@
 import os
-x=os.getenv("x")
-print("entered int is"+ str(x))
-print("hello")
-l=[1,2,3]
-l.reverse()
-print(l)
+rows = 6
+for i in range(1, rows):
+    num = 1
+    for j in range(rows, 0, -1):
+        if j > i:
+            print(" ", end=' ')
+        else:
+            print(num, end=' ')
+            num += 1
+    print("")
